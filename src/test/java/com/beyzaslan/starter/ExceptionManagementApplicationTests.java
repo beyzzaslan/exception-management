2f7fd0ecb9919c332be40d346918e729f0ec8680
1beyzaslan <beyza_aslan_8915@hotmail.com>
2025-08-31 15:34:42 +0300

    JUnit

--- src/test/java/com/beyzaslan/starter/ExceptionManagementApplicationTests.java
+++ src/test/java/com/beyzaslan/starter/ExceptionManagementApplicationTests.java
@@ -1,13 +1,38 @@
 package com.beyzaslan.starter;
 
+import com.beyzaslan.dto.DtoEmployee;
+import com.beyzaslan.model.Employee;
+import com.beyzaslan.service.IEmployeeService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = {ExceptionManagementStarter.class})
 class ExceptionManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private IEmployeeService employeeService;
+
+    @BeforeEach
+    public void beforeEach(){
+        System.out.println("beforeEach çalıştı...");
+    }
+
+    @Test
+    public void testFindEmployeeById() {
+        DtoEmployee employee = employeeService.findEmployeeById(4L);
+      //assertNotNull(4);//bulunup bulunmadığı kontrolunu yapıyoruz
+        assertEquals(4, 4);
+    }
+
+    @AfterEach
+    public  void afterEach(){
+        System.out.println("AfterEach çalıştı...");
+    }
 
 }
